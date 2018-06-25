fc84f703fd52e62c40c7fbfc809d46c3bab96e6c
Justin McCoy <justin.mccoy@us.ibm.com>
2018-06-25 20:12:17 +0100

    Updated with variables for WML services

--- predict.py
+++ predict.py
@@ -5,9 +5,14 @@ import urllib3, requests, json
 
 wml_credentials={
   "url": "https://ibm-watson-ml.mybluemix.net",
-  "username": "*****",
-  "password": "*****"
+  "username": "***",
+  "password": "***"
 }
+
+# Update the following variables with the values of your WML instance, published model, and deployed model
+wml_instance = '***'
+wml_published_model = '***'
+wml_deployed_model = '***'
 
 headers = urllib3.util.make_headers(basic_auth='{username}:{password}'.format(username=wml_credentials['username'], password=wml_credentials['password']))
 url = '{}/v3/identity/token'.format(wml_credentials['url'])
@@ -22,7 +27,8 @@ array_of_values_to_be_scored = [93,22,163,25,49,'F','N','N',110]
 # NOTE: manually define and pass the array(s) of values to be scored in the next line
 payload_scoring = {"fields": ["AVGHEARTBEATSPERMIN", "PALPITATIONSPERDAY", "CHOLESTEROL", "BMI", "AGE", "SEX", "FAMILYHISTORY", "SMOKERLAST5YRS", "EXERCISEMINPERWEEK"], "values": [array_of_values_to_be_scored]}
 
-response_scoring = requests.post('https://ibm-watson-ml.mybluemix.net/v3/wml_instances/8da4912b-9ce2-42bc-a31b-14433f8c930d/published_models/acd936c8-ffe5-44fc-9e34-7a128213496e/deployments/f5f3ea67-0a2e-4517-ba70-9e23299a38c4/online', json=payload_scoring, headers=header)
+response_scoring = requests.post('https://ibm-watson-ml.mybluemix.net/v3/wml_instances/{instance}/published_models/{model}/deployments/{deployment}/online'
+  .format(instance=wml_instance, model=wml_published_model, deployment=wml_deployed_model), json=payload_scoring, headers=header)
 
 print("----Scoring response----")
 parsed = json.loads(response_scoring.text)
